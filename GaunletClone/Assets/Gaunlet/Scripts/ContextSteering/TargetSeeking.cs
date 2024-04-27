8d43aed001fa00cff8a492b41b56124e20366509
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2024-04-27 15:44:34 -0700

    Player seek selects closest target

--- GaunletClone/Assets/Gaunlet/Scripts/ContextSteering/TargetSeeking.cs
+++ GaunletClone/Assets/Gaunlet/Scripts/ContextSteering/TargetSeeking.cs
@@ -20,31 +20,40 @@ public class TargetSeeking : SteeringContext
     {
         sphereDetector.Detect(ref data);
 
-        foreach (Vector3 target in data.targets)
+        // Pick closest target
+        float distance = float.MaxValue;
+        Vector3 direction;
+        foreach (Vector3 target in data.targets) 
         {
-            Vector3 direction = (target - transform.position);
-            float distance = direction.magnitude;
+            direction = (target - transform.position);
 
-            direction = direction.normalized;
-            for(int i = 0; i < Compass.Length; i++)
+            if(direction.magnitude < distance)
             {
-                // The further compass direction can cast without hitting the environment, the more strongly we should weight that direction.
-                // In this case the the max distance of a cast should be the distance to target.
-                RaycastHit hit;
-                string[] layers = { "Default" };
-                float visibility = 1;
-                if (Physics.Linecast(transform.position, transform.position + compass[i] * distance, out hit, LayerMask.GetMask(layers)))
-                {
-                    visibility = Mathf.Clamp(hit.distance / distance, 0, 1);
-                }
+                distance = direction.magnitude;
+                data.target = target;
+            }
+        }
 
-                float value = visibility + Vector3.Dot(compass[i], direction);
+        // Weigh the target
+        direction = (data.target - transform.position);
+        direction = direction.normalized;
+        for(int i = 0; i < Compass.Length; i++)
+        {
+            // The further compass direction can cast without hitting the environment, the more strongly we should weight that direction.
+            // In this case the the max distance of a cast should be the distance to target.
+            RaycastHit hit;
+            string[] layers = { "Default" };
+            float visibility = 1;
+            if (Physics.Linecast(transform.position, transform.position + compass[i] * distance, out hit, LayerMask.GetMask(layers)))
+            {
+                visibility = Mathf.Clamp(hit.distance / distance, 0, 1);
+            }
 
-                if (value > data.interest[i])
-                {
-                    data.interest[i] = value;
-                    data.target = target;
-                }
+            float value = visibility + Vector3.Dot(compass[i], direction);
+
+            if (value > data.interest[i])
+            {
+                data.interest[i] = value;
             }
         }
     }
